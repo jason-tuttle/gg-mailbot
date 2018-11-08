b8ed85282657f85543154fe883974ac28d14eccf
Jason Tuttle <jason.tuttle@greenlight.guru>
2018-11-08 14:19:29 -0500

    add dialog for list of emails

--- emails.js
+++ emails.js
@@ -6,20 +6,36 @@ const baseFilePath = '/var/tmp/emails';
 module.exports = {
   list: [],
 
-  find: function(info) {
-    const user = info.split(' ')[0];
-    const timespan = info.split(' ')[1];
+  findAll: function(user, timespan) {
     const now = Date.now();
+    const cutoff = now - (timespan * 1000 * 60);
+    console.log(`Now: ${now}, Cutoff: ${cutoff}`);
+    let results = [];
+
+    if (!user) {
+      return;
+    } else {
+      var filelist = fs.readdirSync(baseFilePath);
+      var filtered = filelist.filter(file => file.startsWith(user) && file.endsWith('.json'));
+      var filteredWithData = filtered.map(file => this.buildFileInfo(file));
+      results = filteredWithData.filter(file => Date.parse(file.created) >= cutoff);
+    }
+    return results;
+  },
+
+  find: function(user, timespan) {
+    const now = Date.now();
+    const cutoff = now - (timespan * 1000 * 60);
 
     if (!user) {
       return;
     } else {
       var filelist = fs.readdirSync(baseFilePath);
       // var file = filelist.filter(item => item.startsWith(user) && item.endsWith('.json'));
+
       var file = filelist.find(item => item.startsWith(user) && item.endsWith('.json'));
       var data = this.buildFileInfo(file);
 
-      console.log(data);
       // return filelist.find(item => item.startsWith(user) && item.endsWith('.json'));
       return this.buildFileResponse(data);
     }
@@ -30,31 +46,56 @@ module.exports = {
     const file = JSON.parse(fs.readFileSync(baseFilePath + '/' + filename));
     return {
       name: filename,
+      id: file.responseId,
       created: fileInfo.birthtime,
-      contents: file,
+      contents: file.json,
     };
+  },
+
+  replyDialog: function(bot, message, options = {}) {
+    const results = this.findAll(options.user, options.time);
+
+    if (results.length) {
+      const selectResults = results.map(result => (
+        {
+          label: result.contents.Message.Subject.Data,
+          value: result.id,
+        }
+      ));
+
+      var dialog = bot.createDialog(
+        'Emails I found:',
+        'get_email',
+        'View',
+      ).addText('User Email', 'email', options.user)
+        .addSelect('Email Subject','subject','subject', selectResults);
+
+      bot.replyWithDialog(message, dialog.asObject());
+    } else {
+      bot.replyPrivateDelayed(message, {text: "Shit, I couldn't find anything."});
+    }
   },
 
   buildFileResponse: function (fileInfo) {
     var attachment = {
       title: 'File Info',
       title_link: 'Link:',
-      color: '#2C3E50',
+      color: '#31D57C',
       fields: [],
     };
 
     if (fileInfo.contents) {
       attachment.fields.push({
-        title: 'To Email',
-        value: fileInfo.contents.json.Destination.ToAddresses.toString(),
+        title: 'To:',
+        value: fileInfo.contents.Destination.ToAddresses.toString(),
       });
       attachment.fields.push({
-        title: 'Sent',
-        value: moment.utc(fileInfo.created).calendar(),
+        title: 'Sent:',
+        value: moment(fileInfo.created).calendar(),
       });
       attachment.fields.push({
         title: 'Subject:',
-        value: fileInfo.contents.json.Message.Subject.Data,
+        value: fileInfo.contents.Message.Subject.Data,
       });
     }
     return attachment;
